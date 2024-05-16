aec9378a726325bd27ed5b5d82dfedfe0ef03d10
szafranji <silvercrocus@proton.me>
2024-05-16 21:37:02 +0200

    delete recognize_func(just don't need it) now all program's logic will be in run_cmd function

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
-void run_cmd(char *cmd) {
-    
-}
+// at this moment cr0_sh will contain 3 commands: ls, cat, cp //
 
-void recognize_cmd(char *cmd)  {
-
+void run_cmd(char *cmd)  {
+    if(strcmp("ls", cmd) == 0) {
+        system("ls"); // it is not permanent solution
+    }
 }
 
 int main(int argc, char **argv) {
@@ -15,6 +16,8 @@ int main(int argc, char **argv) {
     char line[256];
     printf("cr0$ > ");
     while(fgets(line, sizeof(line), stdin)) {
+        line[strcspn(line, "\n")] = 0;
+        run_cmd(line);
         printf("cr0$ > ");
     }
 }
