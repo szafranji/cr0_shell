daca450ea86acc4f99249ad61522d559e363253b
szafranji <silvercrocus@proton.me>
2024-05-18 21:24:50 +0200

    started implementation of command parser function

--- main.c
+++ main.c
@@ -8,13 +8,24 @@
 #define CYAN "\x1b[36m"
 #define RESET "\x1b[0m"
 
+int words_counter(const char *str) {
+    int space_count = 0;
+    for(int i = 0; str[i] != '\0'; i++) {
+        if(str[i] == ' ')
+            space_count++;
+    }
+    space_count++;
+
+    return space_count;
+}
 
 int is_file_hidden(const char *str) {
     return str[0]=='.';
 }
 
-void cmd_parser(const char *line) {
-    
+void cmd_parser(char *line) {
+    int words_num = words_counter(line);
+    printf("%d \n", words_num);
 }
 
 void ls() {
@@ -22,7 +33,6 @@ void ls() {
     struct dirent *dir_entry;
 
     current_directory = opendir(".");
-    
     if(current_directory != NULL) {
         while((dir_entry = readdir(current_directory)) != NULL) {
             if(is_file_hidden(dir_entry->d_name))
@@ -53,7 +63,8 @@ int main(int argc, char **argv) {
     char line[256];
     do {
         line[strcspn(line, "\n")] = 0;
-        run_cmd(line);
+    //    run_cmd(line);
         printf("cr0$ > ");
+        cmd_parser(line) ;
     } while(fgets(line, sizeof(line), stdin));
 }
