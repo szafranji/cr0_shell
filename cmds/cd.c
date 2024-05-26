1f830327a41e01ca02067c6aab4b32973a734dd8
szafranji <silvercrocus@proton.me>
2024-05-26 22:16:13 +0200

    Now user can cd to home directory.

--- cmds/cd.c
+++ cmds/cd.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <pwd.h>
+
+void cd_main(const char *arg) {
+    int ret;
+    if((ret = chdir(arg)) != 0)
+        perror("cd");
+}
+
+void cd_HOME() {
+    struct passwd *pw = getpwuid(getuid());
+    const char *home_addr = pw->pw_dir;
+    
+    cd_main(home_addr);
+}
+
 
 void cd(const char *arg) {
-    int ret;
-    if((ret = chdir(arg)) != 0) {
-        perror("cd");
-    }
+    if(arg == NULL)
+        cd_HOME();
+    else
+        cd_main(arg);
 }
