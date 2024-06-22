f10548ba295aaa376e3c255a91f5f102b4eae301
szafranji <silvercrocus@proton.me>
2024-06-22 19:30:51 +0200

    Add checking function if arg is file.

--- cmds/echo.c
+++ cmds/echo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
 
 void echo_main(const char *line) {
     printf("%s \n", line);
@@ -11,4 +12,11 @@ void echo_parse(const char *line) {
 
 void echo(const char *line) {
     echo_main(line);
+}
+
+int check_if_arg_gile(const char *arg) {
+    struct stat sb;
+    stat(arg, &sb);
+
+    return S_ISREG(sb.st_mode);
 }
--- cmds/echo.h
+++ cmds/echo.h
@@ -3,5 +3,6 @@
 
 void echo(const char *);
 void echo_parse(const char *);
-void echo_main(const char *)
+void echo_main(const char *);
+int check_if_arg_file(const char *);
 #endif
