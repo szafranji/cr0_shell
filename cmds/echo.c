c43eb696490ff1ff052786e74291fa7998494d4a
szafranji <silvercrocus@proton.me>
2024-06-23 14:22:01 +0200

    Continue working on the echo function.

--- cmds/echo.c
+++ cmds/echo.c
@@ -2,16 +2,16 @@
 #include <string.h>
 #include <sys/stat.h>
 
+int check_gt_symbol(const char *arg) {
+    for(int i = 0; arg[i] != '\0'; i++) {
+        if(arg[i] == '>')
+            return 0;
+    }
+    return 1;
+}
+
 void echo_main(const char *line) {
     printf("%s \n", line);
-}
-
-void echo_parse(const char *line) {
-
-}
-
-void echo(const char *line) {
-    echo_main(line);
 }
 
 int check_if_arg_gile(const char *arg) {
@@ -19,4 +19,13 @@ int check_if_arg_gile(const char *arg) {
     stat(arg, &sb);
 
     return S_ISREG(sb.st_mode);
+}
+
+void echo_parse(const char *line) {
+    if(check_gt_symbol(line) != 0 && check_if_arg_gile(line) != 1)
+        echo_main(line);
+}
+
+void echo(const char *line) {
+    echo_parse(line);
 }
--- cmds/echo.h
+++ cmds/echo.h
@@ -5,4 +5,5 @@ void echo(const char *);
 void echo_parse(const char *);
 void echo_main(const char *);
 int check_if_arg_file(const char *);
+int check_gt_symbol(const char *);
 #endif
